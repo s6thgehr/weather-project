0c9a1ac2f90cac981bf459a2ec86a76481e7acd8
Thomas Gehrmann <s6thgehr@gmail.com>
2022-06-10 09:27:02 +0200

    Finish weather project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const unit = "metric";
+  const apiKey = "03e11a73f8ef31e80a783cb5c6363070";
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=${unit}&q=${query}&appid=${apiKey}`;
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      console.log(imageURL);
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in " + query + " is " + temp + " Celsius</h1>"
+      );
+      res.write(`<img src=${imageURL}>`);
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
